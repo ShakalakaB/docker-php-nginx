836061505798809f31e2b4bd3a0d72f0c3fdfb9b
aldora <aldora988@gmail.com>
2022-10-27 21:20:29 +0800

    Successfully write

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
 print "<h1 style='text-align: center'>Hello</h1>";
+
+print "<h1 style='text-align: center'>Writing log...</h1>";
+if (!is_dir("log")) {
+    mkdir("log");
+}
+print_r("dddd");
+error_log("Test writing permission\n", 3, "./log/error.log");
+
