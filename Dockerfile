68ecdc77c349f5d712cc50e68a5ca069c75d8152
aldora <aldora988@gmail.com>
2022-10-29 21:18:43 +0800

    Linux/MacOS: log written ok with add new user

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,18 @@
 FROM php:7.4-fpm
 
+ENV UID=1000
+ENV USER_NAME=ec2-user
+
+ENV GID=1000
+ENV GROUP_NAME=ec2-user
+
 WORKDIR /var/www
 
-ADD ./  /var/www
+RUN addgroup -gid ${GID} --system ${GROUP_NAME}
+RUN adduser --system --ingroup ${GROUP_NAME} -shell /bin/sh --uid ${UID} --disabled-password ${USER_NAME}
 
-RUN chown -R www-data:www-data /var/www
+ADD ./src/  /var/www
+
+RUN chown -R ${USER_NAME}:${GROUP_NAME} /var/www
+
+USER ${USER_NAME}
---
+++ src/public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+print "<h1 style='text-align: center'>Hello</h1>";
+
+print "<h1 style='text-align: center'>Writing log...</h1>";
+
+$user = shell_exec('whoami');
+print "<h1 style='text-align: center'>Current user: {$user}</h1>";
+
+if (!is_dir("../log")) {
+    mkdir("../log");
+}
+
+if (error_log("Test writing permission\n", 3, "../log/error.log")) {
+    print "<h1 style='text-align: center'>Log Written</h1>";
+} else {
+    print "<h1 style='text-align: center'>Log Writing Failed</h1>";
+}
+
